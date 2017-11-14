a21d537cc94a734428f4b2a27a176418693c6d1a
LiKang <LiKangSkying@outlook.com>
2017-11-14 20:19:39 +0800

    201711142019

--- Home/The Most Wanted Letter.py
+++ Home/The Most Wanted Letter.py
@@ -3,7 +3,10 @@ def checkio(text):
     #replace this for solution
     import  string
 
+    # lower函数,小写化
     text = text.lower()
+    # max 函数,根据text.count 关键字来返回最大的数
+    # 如果相同根据ascii_lowercase 来选择
     return max(string.ascii_lowercase,key=text.count)
 
 
@@ -22,8 +25,45 @@ if __name__ == '__main__':
 #######################
 from collections import Counter
 def checkio(text):
+    # count 字典
     count = Counter([x for x in text.lower() if x.isalpha()]) #count字典
     print(count)
     m = max(count.values())
     print(m)
     return sorted([x for (x, y) in count.items() if y == m])[0]
+######################
+def checkio(text):
+    x=len(text)
+    text=text.lower()
+    pom="abcdefghijklmnopqrstuvwxyz"
+    y=len(pom)
+    max=0
+    pom2=""
+    ile=0
+    for i in range(y):
+        ile=text.count(pom[i])
+        if (ile>max):
+            max=ile
+            pom2=pom[i]
+    return pom2
+########################
+from collections import defaultdict as defaultdict
+from collections import OrderedDict as OrderedDict
+from operator import itemgetter as itemgetter
+
+def checkio(text):
+
+    # convert to lower case
+    text = text.lower()
+    text = ''.join(filter(str.isalpha, text))
+
+    # make a structure for unique letter with counts
+    holder = defaultdict(int)
+    for letter in text: holder[letter] += 1
+
+    # sort letters by alphabet
+    holder = OrderedDict(sorted(holder.items()))
+
+    most_frequent_letter = [k for k, v in holder.items() if v == max(holder.values())][0]
+
+    return most_frequent_letter
